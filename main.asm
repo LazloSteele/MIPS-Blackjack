cdb7091944e1f88cc7d121da697f55f7a0d46f2b
Lazlo Steele <lazlosteele@gmail.com>
2024-12-01 08:38:34 -0700

    all functionality built except shuffle

--- main.asm
+++ main.asm
@@ -3,7 +3,17 @@
 player_hand_msg: 	.asciiz "\nPlayer Hand: "
 dealer_hand_msg: 	.asciiz "\nDealer Hand: "
 point_count_msg: 	.asciiz "\nTotal Point Count: "
-action_prompt:		.asciiz "\nHit [0] or Stand [1] > " 
+action_prompt:		.asciiz "\nHit [0] or Stand [1] > "
+blackjack:			.asciiz "\nBLACKJACK!"
+bust:				.asciiz "\nBUST!"
+win_msg:			.asciiz "\nYou win!"
+lose_msg:			.asciiz "\nYou lose!"
+push_msg:			.asciiz "\nPUSH!\nNobody wins..."
+repeat_msg:			.asciiz "\nGo again? Y/N > "
+invalid_msg:		.asciiz "\nInvalid input. Try again!\n"
+bye: 				.asciiz "\nToodles! ;)"
+
+_XX:				.asciiz "XX"
 
 _AD:				.asciiz "AD"
 _2D:				.asciiz "2D"
@@ -81,22 +91,25 @@ player_cards:		.word	0
 dealer_cards:		.word	0
 player_score:		.word 	0
 dealer_score:		.word	0
+dealer_blind:		.word	1
 buffer:				.space	2
 
 					.globl		main
 	
 					.text
 main:
+	jal		fy_shuffle
+
 	li		$a0, 0
 	jal		draw
-	jal		draw
-	jal		draw
-	jal		draw
 	li		$a0, 1
+	jal		draw
+	li		$a0, 0
 	jal		draw
 	li		$a0, 1
 	jal		draw
 
+	jal		check_blackjack
 
 	li		$a0, 0
 	jal		display_hand
@@ -104,10 +117,43 @@ main:
 	li		$a0, 1
 	jal		display_hand
 
+	jal		prompt_user_turn
 
-	li $v0, 10
-	syscall
+	la		$t0, dealer_blind
+	li		$t1, 0
+	sw		$t1, 0($t0)
+
+	li		$a0, 0
+	jal		display_hand
 	
+	li		$a0, 1
+	jal		display_hand
+	
+	la		$t0, player_score
+	lw		$t0, 0($t0)
+	la		$t1, dealer_score
+	lw		$t1, 0($t1)
+	
+	beq		$t0, $t1, push
+	bgt		$t0, $t1, player_win
+	
+	dealer_win:
+		li		$v0, 4
+		la		$a0, lose_msg
+		syscall
+		
+		j		again
+
+	player_win:
+		li		$v0, 4
+		la		$a0, win_msg
+		syscall
+	
+		j		again
+
+fy_shuffle:
+	jr		$ra
+
 draw:
 	la		$t0, deck					# get the deck
 	la		$t1, deck_index				# where is the current top of the deck
@@ -142,10 +188,12 @@ draw:
 		jr		$ra						#
 										#
 display_hand:
+	move	$s0, $a0							# whose hand is it?
+												#
 	li		$t0, 0								# iterator for cards in hand
 	li		$t5, 0								# for counting points
 	li		$t8, 0								# number of aces
-	bnez	$a0, display_dealer_hand
+	bnez	$s0, display_dealer_hand
 	
 	display_player_hand:
 		lw		$t1, player_cards
@@ -155,10 +203,12 @@ display_hand:
 		la		$a0, player_hand_msg
 	
 		j		display_loop
+		
 	display_dealer_hand:
 		lw		$t1, dealer_cards
 		la		$t2, dealer_hand
 		la		$t7, dealer_score
+		lw		$t9, dealer_blind
 		
 		la		$a0, dealer_hand_msg
 	
@@ -168,36 +218,48 @@ display_hand:
 		syscall
 
 		for_card_in_hand:
-			lw		$t3, 0($t2)					# current card
-			mul		$t3, $t3, 4					# word offset
+			beqz	$s0, not_blind_dealer
+			beqz	$t9, not_blind_dealer
+			bnez	$t0, not_blind_dealer
+			
+			la		$a0, _XX
+			li		$v0, 4
+			syscall
+
+			j	normal_print			
+			
+			not_blind_dealer:
+				lw		$t3, 0($t2)					# current card
+				mul		$t3, $t3, 4					# word offset
+													#
+				la		$t4, card_names				# load table of card name pointers
+				add		$t4, $t4, $t3				# move to index of current card
+													#
+				lw		$a0, 0($t4)					# load the address of the current card name pointer
+				li		$v0, 4						#
+				syscall								# and print the card
 												#
-			la		$t4, card_names				# load table of card name pointers
-			add		$t4, $t4, $t3				# move to index of current card
+				la		$t4, card_values			# load table of card value pointers
+				add		$t4, $t4, $t3				# move to index of current card
 												#
-			lw		$a0, 0($t4)					# load the address of the current card name pointer
-			li		$v0, 4						#
-			syscall								# and print the card
-												#
-			la		$t4, card_values			# load table of card value pointers
-			add		$t4, $t4, $t3				# move to index of current card
-												#
-			lw		$t6, 0($t4)					# load the address of current card value pointer
-			bne		$t6, 11, no_ace				# if not 11 then not an ace
-			addi	$t8, $t8, 1					# add one ace to the hand count
+				lw		$t6, 0($t4)					# load the address of current card value pointer
+				bne		$t6, 11, no_ace				# if not 11 then not an ace
+				addi	$t8, $t8, 1					# add one ace to the hand count
 												#
 			no_ace:								#
 				add		$t5, $t5, $t6			# add that to current entity score
 												#
-			beq		$t0, $t1, display_done		# terminate here to avoid trailing ", "
+			normal_print:						#
+				beq		$t0, $t1, display_done		# terminate here to avoid trailing ", "
 												#
-			li		$v0, 11						#
-			li		$a0, ','					#
-			syscall								#
-			li		$a0, ' '					#
-			syscall								#
+				li		$v0, 11					#
+				li		$a0, ','					#
+				syscall								#
+				li		$a0, ' '					#
+				syscall								#
 												#
-			addi	$t0, $t0, 1					# iterate the card counter
-			addi	$t2, $t2, 4					# iterate the current card pointer address
+				addi	$t0, $t0, 1					# iterate the card counter
+				addi	$t2, $t2, 4					# iterate the current card pointer address
 												#
 			j for_card_in_hand					# do the timewarp
 												#
@@ -224,4 +286,257 @@ display_hand:
 												#												#
 			jr		$ra							#
 												#
+prompt_user_turn:
+	la		$a0, action_prompt
+	li		$v0, 4
+	syscall
 	
+	la		$a0, buffer
+	li		$a1, 2
+	li		$v0, 8
+	syscall
+	
+	la		$t0, buffer
+	lb		$t0, 0($t0)
+
+	beq		$t0, '0', hit
+	beq		$t0, '1', stand
+	
+	la		$a0, invalid_msg
+	li		$v0, 4
+	syscall
+	
+	j		prompt_user_turn
+	
+	hit:
+		move	$s1, $ra
+		li		$a0, 0
+		jal		draw
+		
+		li		$a0, 0
+		jal		display_hand
+		
+		la		$a0, player_score
+		jal		check_bust
+		bnez	$v0, player_bust
+		
+		move	$ra, $s1
+		
+		j		prompt_user_turn
+		
+	stand:
+		jr		$ra
+
+check_blackjack:
+	li		$t0, 0
+	li		$t1, 0
+	
+	la		$t2, player_hand				#
+	li		$t3, 0							# player score
+	
+	player_check_blackjack:					
+		lw		$t4, 0($t2)
+		add		$t3, $t3, $t4
+		lw		$t4, 4($t2)
+		add		$t3, $t3, $t4
+		seq		$t0, $t3, 21
+				
+	la		$t2, player_hand				#
+	li		$t3, 0							# dealer score
+	dealer_check_blackjack:
+		lw		$t4, 0($t2)
+		add		$t3, $t3, $t4
+		lw		$t4, 4($t2)
+		add		$t3, $t3, $t4
+		seq		$t1, $t3, 21
+	check_blackjack_done:
+		bnez	$t0, player_blackjack
+		bnez	$t1, dealer_blackjack
+		
+		jr		$ra
+	push:
+		li		$v0, 4
+		la		$a0, push_msg
+		syscall
+		
+		j		again
+		
+	player_blackjack:
+		beq		$t0, $t1, push
+		li		$v0, 4
+		la		$a0, blackjack
+		syscall
+
+		la		$a0, win_msg
+		syscall
+		
+		j		again
+	
+	dealer_blackjack:
+		li		$t2, 0
+		la		$t3, dealer_blind
+		sw		$t2, 0($t3)
+		
+		li		$a0, 1
+		move	$s0, $ra
+		jal		display_hand
+		move	$ra, $s0
+	
+		li		$v0, 4
+		la		$a0, blackjack
+		syscall
+
+		la		$a0, lose_msg
+		syscall
+		
+		j		again
+
+check_bust:
+	lw		$t0, 0($a0)
+	li		$v0, 0
+	
+	ble		$t0, 21, check_done
+	
+	li		$v0, 1
+	
+	check_done:
+		jr		$ra
+		
+player_bust:
+	la		$a0, bust
+	li		$v0, 4
+	syscall
+	
+	la		$a0, lose_msg
+	syscall
+	
+	j		again
+	
+dealer_bust:
+	la		$a0, bust
+	li		$v0, 4
+	syscall
+	
+	la		$a0, win_msg
+	syscall
+	
+	j		again
+	
+####################################################################################################
+# macro: upper
+# purpose: to make printing messages more eloquent
+# registers used:
+#	$t0 - string to check for upper case
+#	$t1 - ascii 'a', 'A'-'Z' is all lower value than 'a'
+# variables used:
+#	%message - message to be printed
+####################################################################################################		
+upper:							#
+	move $t0, $a0				# load the buffer address
+	li $t1, 'a'					# lower case a to compare
+	upper_loop:					#
+		lb $t2, 0($t0)			# load next byte from buffer
+		blt $t2, $t1, is_upper	# bypass uppercaserizer if character is already upper case (or invalid)
+		to_upper:				# 
+			subi $t2, $t2, 32	# Convert to uppercase (ASCII difference between 'a' and 'A' is 32)
+		is_upper:				#
+			sb $t2, 0($t0)		# store byte
+		addi $t0, $t0, 1		# next byte
+		bne $t2, 0, upper_loop	# if not end of buffer go again!
+	jr $ra						#
+								#
+####################################################################################################
+# function: again
+# purpose: to user to repeat or close the program
+# registers used:
+#	$v0 - syscall codes
+#	$a0 - message storage for print and buffer storage
+#	$t0 - stores the memory address of the buffer and first character of the input received
+#	$t1 - ascii 'a', 'Y', and 'N'
+####################################################################################################
+again:							#		
+	la $a0, repeat_msg			#
+	li $v0, 4					#
+	syscall						#
+								#
+	la $a0, buffer				#
+	la $a1, 2					#
+	li $v0, 8					#
+	syscall						#
+								#
+	la $a0, buffer				#
+	jal upper					# load the buffer for string manipulation
+								#
+	la $t0, buffer				#
+	lb $t0, 0($t0)				#
+	li $t1, 'Y'					# store the value of ASCII 'Y' for comparison
+	beq $t0, $t1, reenter		# If yes, go back to the start of main
+	li $t1, 'N'					# store the value of ASCII 'N' for comparison
+	beq $t0, $t1, end			# If no, goodbye!
+								#
+	again_invalid:				#
+		la $a0, invalid_msg		#
+		li $v0, 4				#
+		syscall					#
+								#
+		j again					#
+								#
+####################################################################################################
+# function: reenter
+# purpose: to clear program to default values for re-entry
+# registers used:
+#	$v0 - syscall codes
+#	$a0 - message addresses
+####################################################################################################	
+reenter:									#
+	la		$t0, dealer_blind
+	li		$t1, 1
+	sw		$t1, 0($t0)
+
+	la		$t0, deck_index
+	li		$t1, 0
+	sw		$t1, 0($t0)
+	
+	la		$t0, player_cards
+	sw		$t1, 0($t0)
+	la		$t0, player_score
+	sw		$t1, 0($t0)
+	
+	la		$t0, dealer_cards
+	sw		$t1, 0($t0)
+	la		$t0, dealer_score
+	sw		$t1, 0($t0)
+	
+	li		$t2, 0
+	la		$t3, player_hand
+	la		$t4, dealer_hand
+	reset_hands:
+		sw		$t1, 0($t3)
+		sw		$t1, 0($t4)
+		
+		addi	$t3, $t3, 4
+		addi	$t4, $t4, 4
+		
+		addi	$t2, $t2, 1
+		
+		blt		$t2, 11, reset_hands
+	
+	j	main								# truly do the timewarp again
+											#
+####################################################################################################
+# function: end
+# purpose: to eloquently terminate the program
+# registers used:
+#	$v0 - syscall codes
+#	$a0 - message addresses
+####################################################################################################	
+end:	 					#
+	la		$a0, bye		#
+	li		$v0, 4			#
+	syscall					#
+							#
+	li 		$v0, 10			# system call code for returning control to system
+	syscall					# GOODBYE!
+							#
+####################################################################################################	
+
